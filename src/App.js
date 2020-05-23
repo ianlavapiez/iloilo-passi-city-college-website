d9b2b6c94079c4d9b531a76f8a75207dde277aa2
ianlavapiez <ianlavapiez@gmail.com>
2020-05-23 16:38:56 +0800

    [src]: modified App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
-import React from 'react'
+import React, { useEffect, lazy, Suspense } from 'react'
+import { Switch, Route, Redirect } from 'react-router-dom'
+import { connect } from 'react-redux'
+import { createStructuredSelector } from 'reselect'
+
+import { checkUserSession } from './redux/user/user.actions'
 
 function App() {
   return <div className='App'>hello world</div>
