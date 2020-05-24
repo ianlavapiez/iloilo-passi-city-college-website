4e46a8e0cd7834773977fd2aa9e4402443bf69a6
ianlavapiez <ianlavapiez@gmail.com>
2020-05-24 18:38:02 +0800

    [routes]: added additional routes for stories and privacy policy

--- src/routes/public.routes.js
+++ src/routes/public.routes.js
@@ -5,8 +5,10 @@ import AboutPage from '../pages/public/about/about.page'
 import ContactPage from '../pages/public/contact/contact.page'
 import HomePage from '../pages/public/home/home.page'
 import OnlineReviewPage from '../pages/public/online-review/online-review.page'
+import PrivacyPolicyPage from '../pages/public/privacy-policy/privacy-policy.page'
 import ProgramsPage from '../pages/public/programs/programs.page'
 import RegistrationPage from '../pages/public/registration/registration.page'
+import StoriesPage from '../pages/public/stories/stories.page'
 
 const PublicRoutes = () => {
   return (
@@ -15,8 +17,10 @@ const PublicRoutes = () => {
       <Route exact path='/about' component={AboutPage} />
       <Route exact path='/contact' component={ContactPage} />
       <Route exact path='/online-review' component={OnlineReviewPage} />
+      <Route exact path='/privacy-policy' component={PrivacyPolicyPage} />
       <Route exact path='/programs' component={ProgramsPage} />
       <Route exact path='/registration' component={RegistrationPage} />
+      <Route exact path='/stories' component={StoriesPage} />
     </Route>
   )
 }
