a2eb32371c00061ec20b6026e917deda13be2ec4
ianlavapiez <ianlavapiez@gmail.com>
2020-05-24 18:37:42 +0800

    [pages]: added about, features, and stories section

--- src/pages/public/home/home.page.jsx
+++ src/pages/public/home/home.page.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import React, { Fragment } from 'react'
+
+import Header from '../../../components/home/header/header.component'
+import Footer from '../../../components/common/footer/footer.component'
+import SectionAbout from '../../../components/home/section-about/section-about.component'
+import SectionFeatures from '../../../components/home/section-features/section-features.component'
+import SectionStories from '../../../components/home/section-stories/section-stories.component'
 
 const HomePage = () => {
-  return <div>Home Page</div>
+  return (
+    <Fragment>
+      <Header />
+      <main>
+        <SectionAbout />
+        <SectionFeatures />
+        <SectionStories />
+      </main>
+      <Footer />
+    </Fragment>
+  )
 }
 
 export default HomePage
