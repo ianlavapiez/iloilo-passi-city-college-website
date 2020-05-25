a837a6ec93c096c438f4b5476c8d1adf010c05dd
ianlavapiez <ianlavapiez@gmail.com>
2020-05-25 23:15:22 +0800

    [routes]: added createBrowserHistory

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,18 +1,21 @@
 import React from 'react'
-import { Route } from 'react-router-dom'
+import { Router } from 'react-router-dom'
+import { createBrowserHistory } from 'history'
 
 import AdminRoutes from './admin.routes'
 import PublicRoutes from './public.routes'
 import RARoutes from './ra.routes'
 import StudentRoutes from './student.routes'
 
+const history = createBrowserHistory()
+
 const routes = () => (
-  <Route>
+  <Router history={history}>
     <PublicRoutes />
     <AdminRoutes />
     <RARoutes />
     <StudentRoutes />
-  </Route>
+  </Router>
 )
 
 export default routes
