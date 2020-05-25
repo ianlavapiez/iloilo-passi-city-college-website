f3f2e9523a1d7c5658b909fb59a02f242d411412
ianlavapiez <ianlavapiez@gmail.com>
2020-05-25 23:10:57 +0800

    [components]: added common inputs

---
+++ src/components/common/input/input.component.jsx
@@ -0,0 +1,78 @@
+import React from 'react'
+import classNames from 'classnames'
+import PropTypes from 'prop-types'
+// @material-ui/core components
+import { makeStyles } from '@material-ui/core/styles'
+import { FormControl, InputLabel, Input } from '@material-ui/core/FormControl'
+// @material-ui/icons
+import { Check, Clear } from '@material-ui/icons/Clear'
+// core components
+import inputStyles from 'assets/jss/material-dashboard-react/components/customInputStyle.js'
+
+const useStyles = makeStyles(inputStyles)
+
+export default function CustomInput(props) {
+  const classes = useStyles()
+  const {
+    formControlProps,
+    labelText,
+    id,
+    labelProps,
+    inputProps,
+    error,
+    success,
+  } = props
+
+  const labelClasses = classNames({
+    [' ' + classes.labelRootError]: error,
+    [' ' + classes.labelRootSuccess]: success && !error,
+  })
+  const underlineClasses = classNames({
+    [classes.underlineError]: error,
+    [classes.underlineSuccess]: success && !error,
+    [classes.underline]: true,
+  })
+  const marginTop = classNames({
+    [classes.marginTop]: labelText === undefined,
+  })
+  return (
+    <FormControl
+      {...formControlProps}
+      className={formControlProps.className + ' ' + classes.formControl}
+    >
+      {labelText !== undefined ? (
+        <InputLabel
+          className={classes.labelRoot + labelClasses}
+          htmlFor={id}
+          {...labelProps}
+        >
+          {labelText}
+        </InputLabel>
+      ) : null}
+      <Input
+        classes={{
+          root: marginTop,
+          disabled: classes.disabled,
+          underline: underlineClasses,
+        }}
+        id={id}
+        {...inputProps}
+      />
+      {error ? (
+        <Clear className={classes.feedback + ' ' + classes.labelRootError} />
+      ) : success ? (
+        <Check className={classes.feedback + ' ' + classes.labelRootSuccess} />
+      ) : null}
+    </FormControl>
+  )
+}
+
+CustomInput.propTypes = {
+  labelText: PropTypes.node,
+  labelProps: PropTypes.object,
+  id: PropTypes.string,
+  inputProps: PropTypes.object,
+  formControlProps: PropTypes.object,
+  error: PropTypes.bool,
+  success: PropTypes.bool,
+}
