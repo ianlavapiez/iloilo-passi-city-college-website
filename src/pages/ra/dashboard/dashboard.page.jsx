4e89fee1dcde2aca6c896f5fa91120378fb80ba0
ianlavapiez <ianlavapiez@gmail.com>
2020-05-26 21:41:32 +0800

    [pages]: modified ra pages

--- src/pages/ra/dashboard/dashboard.page.jsx
+++ src/pages/ra/dashboard/dashboard.page.jsx
@@ -1,7 +1,30 @@
 import React from 'react'
+import { Layout } from 'antd'
+
+import Sidebar from '../../../components/ra/sidebar/sidebar.component'
+import Navbar from '../../../components/ra/navbar/navbar.component'
+import RaFooter from '../../../components/ra/footer/footer.component'
+
+const { Content } = Layout
 
 const DashboardPage = () => {
-  return <div>Dashboard Page</div>
+  return (
+    <Layout style={{ minHeight: '100vh' }}>
+      <Sidebar number={'1'} />
+      <Layout className='site-layout'>
+        <Navbar />
+        <Content style={{ margin: '0 16px' }}>
+          <div
+            className='site-layout-background'
+            style={{ padding: 20, minHeight: '85vh' }}
+          >
+            Dashboard Page
+          </div>
+        </Content>
+        <RaFooter />
+      </Layout>
+    </Layout>
+  )
 }
 
 export default DashboardPage
--- src/pages/ra/student-management/student-management.page.jsx
+++ src/pages/ra/student-management/student-management.page.jsx
@@ -1,7 +1,30 @@
 import React from 'react'
+import { Layout } from 'antd'
+
+import Sidebar from '../../../components/ra/sidebar/sidebar.component'
+import Navbar from '../../../components/ra/navbar/navbar.component'
+import RaFooter from '../../../components/ra/footer/footer.component'
+
+const { Content } = Layout
 
 const StudentManagementPage = () => {
-  return <div>Student Management Page</div>
+  return (
+    <Layout style={{ minHeight: '100vh' }}>
+      <Sidebar number={'5'} />
+      <Layout className='site-layout'>
+        <Navbar />
+        <Content style={{ margin: '0 16px' }}>
+          <div
+            className='site-layout-background'
+            style={{ padding: 20, minHeight: '85vh' }}
+          >
+            Student Management Page
+          </div>
+        </Content>
+        <RaFooter />
+      </Layout>
+    </Layout>
+  )
 }
 
 export default StudentManagementPage
