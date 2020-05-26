4ad9f98b2248f54ef16ed464033048e0241f703c
ianlavapiez <ianlavapiez@gmail.com>
2020-05-26 21:40:50 +0800

    [pages]: modified dashboard and statistics and reports admin page

--- src/pages/admin/dashboard/dashboard.page.jsx
+++ src/pages/admin/dashboard/dashboard.page.jsx
@@ -1,7 +1,30 @@
 import React from 'react'
+import { Layout } from 'antd'
+
+import Sidebar from '../../../components/admin/sidebar/sidebar.component'
+import Navbar from '../../../components/admin/navbar/navbar.component'
+import AdminFooter from '../../../components/admin/footer/footer.component'
+
+const { Content } = Layout
 
 const DashboardPage = () => {
-  return <div>Dashboard Page</div>
+  return (
+    <Layout style={{ minHeight: '100vh' }}>
+      <Sidebar number={'1'} />
+      <Layout className='site-layout'>
+        <Navbar />
+        <Content style={{ margin: '0 16px' }}>
+          <div
+            className='site-layout-background'
+            style={{ padding: 20, minHeight: '85vh' }}
+          >
+            Admin Dashboard Page
+          </div>
+        </Content>
+        <AdminFooter />
+      </Layout>
+    </Layout>
+  )
 }
 
 export default DashboardPage
