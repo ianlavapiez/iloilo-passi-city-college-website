5afe1af39bc83f3d2cec79e27ccdac9632832f64
ianlavapiez <ianlavapiez@gmail.com>
2020-05-26 11:13:36 +0800

    [pages]: modified footer import

--- src/pages/public/home/home.page.jsx
+++ src/pages/public/home/home.page.jsx
@@ -1,7 +1,7 @@
 import React, { Fragment } from 'react'
 
 import Header from '../../../components/public/home/header/header.component'
-import Footer from '../../../components/common/footer/public/footer.component'
+import Footer from '../../../components/public/footer/footer.component'
 import SectionAbout from '../../../components/public/home/section-about/section-about.component'
 import SectionFeatures from '../../../components/public/home/section-features/section-features.component'
 import SectionStories from '../../../components/public/home/section-stories/section-stories.component'
