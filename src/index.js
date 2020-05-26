0c542becca73f9ce90cac5e58de35f139a0378a4
ianlavapiez <ianlavapiez@gmail.com>
2020-05-26 21:39:25 +0800

    [index.js]: added antd css

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ import { PersistGate } from 'redux-persist/integration/react'
 import { store, persistor } from './redux/store'
 
 import './css/icon-font.css'
+import 'antd/dist/antd.css'
+
 import App from './App'
 
 ReactDOM.render(
