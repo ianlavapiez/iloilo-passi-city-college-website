6769d6e2f128a4a4005b1232ca4f0f44fc6a0543
ianlavapiez <ianlavapiez@gmail.com>
2020-05-27 14:00:02 +0800

    [pages]: added student admin pages

--- src/pages/student/payment/payment.page.jsx
+++ src/pages/student/payment/payment.page.jsx
@@ -1,7 +1,30 @@
 import React from 'react'
+import { Layout } from 'antd'
+
+import Sidebar from '../../../components/student/sidebar/sidebar.component'
+import Navbar from '../../../components/student/navbar/navbar.component'
+import StudentFooter from '../../../components/student/footer/footer.component'
+
+const { Content } = Layout
 
 const PaymentPage = () => {
-  return <div>Payment Page</div>
+  return (
+    <Layout style={{ minHeight: '100vh' }}>
+      <Sidebar number={'2'} />
+      <Layout className='site-layout'>
+        <Navbar />
+        <Content style={{ margin: '0 16px' }}>
+          <div
+            className='site-layout-background'
+            style={{ padding: 20, minHeight: '85vh' }}
+          >
+            Payment Page
+          </div>
+        </Content>
+        <StudentFooter />
+      </Layout>
+    </Layout>
+  )
 }
 
 export default PaymentPage
