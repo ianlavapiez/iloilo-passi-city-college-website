9e9c6d49c1847009e8dcc143d80b4541508b95c0
ianlavapiez <ianlavapiez@gmail.com>
2020-05-27 13:58:36 +0800

    [routes]: modified student routes

--- src/routes/student.routes.js
+++ src/routes/student.routes.js
@@ -5,14 +5,16 @@ import EnrollmentPage from '../pages/student/enrollment/enrollment.page'
 import LoginPage from '../pages/student/login/login.page'
 import PaymentPage from '../pages/student/payment/payment.page'
 import SchedulePage from '../pages/student/schedule/schedule.page'
+import TutorialPage from '../pages/student/tutorial/tutorial.page'
 
 const StudentRoutes = () => {
   return (
     <Route>
       <Route exact path='/student' component={EnrollmentPage} />
       <Route exact path='/student/login' component={LoginPage} />
-      <Route exact path='/student/payment' component={PaymentPage} />
+      <Route exact path='/student/payments' component={PaymentPage} />
       <Route exact path='/student/schedule' component={SchedulePage} />
+      <Route exact path='/student/tutorials' component={TutorialPage} />
     </Route>
   )
 }
