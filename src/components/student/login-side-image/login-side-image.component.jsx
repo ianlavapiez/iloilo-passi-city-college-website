7f984e89f3cc31ea8bbd6acb78cccdd712e23a67
ianlavapiez <ianlavapiez@gmail.com>
2020-05-27 18:13:40 +0800

    [components]: added student login side image and login component

---
+++ src/components/student/login-side-image/login-side-image.component.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import './login-side-image.styles.scss'
+
+import LoginImage from '../../../assets/login-logo.jpg'
+
+const LoginSideImage = () => {
+  return (
+    <div>
+      <img className='logo-container' src={LoginImage} alt='Login Logo' />
+    </div>
+  )
+}
+
+export default LoginSideImage
