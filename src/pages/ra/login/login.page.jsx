4e5ba46413dda67a89b8c9dba2805edfc26c1a35
ianlavapiez <ianlavapiez@gmail.com>
2020-05-27 18:09:43 +0800

    [pages]: added login page

--- src/pages/ra/login/login.page.jsx
+++ src/pages/ra/login/login.page.jsx
@@ -1,7 +1,22 @@
 import React from 'react'
+import { Row, Col } from 'antd'
+
+import './login.styles.scss'
+
+import LoginForm from '../../../components/ra/login/login.component'
+import LoginSideImage from '../../../components/ra/login-side-image/login-side-image.component'
 
 const LoginPage = () => {
-  return <div>Login Page</div>
+  return (
+    <Row className='container'>
+      <Col span={12}>
+        <LoginSideImage />
+      </Col>
+      <Col span={12}>
+        <LoginForm />
+      </Col>
+    </Row>
+  )
 }
 
 export default LoginPage
