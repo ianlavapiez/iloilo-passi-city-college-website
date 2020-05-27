611055fe90fe93e4f4e945fcc9c8aa029518c6c5
ianlavapiez <ianlavapiez@gmail.com>
2020-05-27 16:46:02 +0800

    [pages]: modified student management page

--- src/pages/ra/student-management/student-management.page.jsx
+++ src/pages/ra/student-management/student-management.page.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Layout, Breadcrumb } from 'antd'
+import { Layout, Breadcrumb, Typography } from 'antd'
 
 import Sidebar from '../../../components/ra/sidebar/sidebar.component'
 import Navbar from '../../../components/ra/navbar/navbar.component'
@@ -8,6 +8,7 @@ import StudentTable from '../../../components/ra/student-table/student-table.com
 import StudentModal from '../../../components/ra/student-modal/student-modal.component'
 
 const { Content } = Layout
+const { Title } = Typography
 
 const StudentManagementPage = () => {
   return (
@@ -16,8 +17,10 @@ const StudentManagementPage = () => {
       <Layout className='site-layout'>
         <Navbar />
         <Layout style={{ padding: '0 24px 24px' }}>
-          <Breadcrumb style={{ margin: '50px 0px 0px 30px' }}>
-            <Breadcrumb.Item>List of Students</Breadcrumb.Item>
+          <Breadcrumb style={{ margin: '40px 0px -20px 30px' }}>
+            <Breadcrumb.Item>
+              <Title level={3}>List of Students</Title>
+            </Breadcrumb.Item>
           </Breadcrumb>
           <Content
             className='site-layout-background'
@@ -31,14 +34,6 @@ const StudentManagementPage = () => {
             <StudentTable />
           </Content>
         </Layout>
-        {/* <Content style={{ margin: '0 16px' }}>
-          <div
-            className='site-layout-background'
-            style={{ padding: 20, minHeight: '85vh' }}
-          >
-            Student Management Page
-          </div>
-        </Content> */}
         <RaFooter />
       </Layout>
     </Layout>
