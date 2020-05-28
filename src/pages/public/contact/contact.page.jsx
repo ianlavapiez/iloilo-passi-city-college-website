0bceab9bba06174370b17b65673d4b72f54207c5
ianlavapiez <ianlavapiez@gmail.com>
2020-05-28 18:29:42 +0800

    [pages]: added window.scrollTo method

--- src/pages/public/contact/contact.page.jsx
+++ src/pages/public/contact/contact.page.jsx
@@ -1,10 +1,14 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useEffect } from 'react'
 
 import Header from '../../../components/public/contact/header/header.component'
 import ContactInformation from '../../../components/public/contact/contact-information/contact-information.component'
 import Footer from '../../../components/public/footer/footer.component'
 
 const ContactPage = () => {
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [])
+
   return (
     <Fragment>
       <Header />
