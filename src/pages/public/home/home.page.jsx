38ccd9d30bbed01697be6cf2e43babda09c3dc81
ianlavapiez <ianlavapiez@gmail.com>
2020-05-28 18:29:30 +0800

    [pages]: added window.scrollTo method

--- src/pages/public/home/home.page.jsx
+++ src/pages/public/home/home.page.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useEffect } from 'react'
 
 import Header from '../../../components/public/home/header/header.component'
 import Footer from '../../../components/public/footer/footer.component'
@@ -7,6 +7,10 @@ import SectionFeatures from '../../../components/public/home/section-features/se
 import SectionStories from '../../../components/public/home/section-stories/section-stories.component'
 
 const HomePage = () => {
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [])
+
   return (
     <Fragment>
       <Header />
