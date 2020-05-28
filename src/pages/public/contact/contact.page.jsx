c9ea6becf8470dc4065e050ab0d7de7d9ec0f6e5
ianlavapiez <ianlavapiez@gmail.com>
2020-05-28 11:24:02 +0800

    [pages]: modified contact page

--- src/pages/public/contact/contact.page.jsx
+++ src/pages/public/contact/contact.page.jsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import React, { Fragment } from 'react'
+
+import Header from '../../../components/public/contact/header/header.component'
+import ContactInformation from '../../../components/public/contact/contact-information/contact-information.component'
+import Footer from '../../../components/public/footer/footer.component'
 
 const ContactPage = () => {
-  return <div>Contact Page</div>
+  return (
+    <Fragment>
+      <Header />
+      <ContactInformation />
+      <Footer />
+    </Fragment>
+  )
 }
 
 export default ContactPage
