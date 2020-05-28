11cd529caf9f5dfb2e3cbf64cfcc5861f967f6c4
ianlavapiez <ianlavapiez@gmail.com>
2020-05-28 18:29:17 +0800

    [pages]: added window.scrollTo method

--- src/pages/public/online-review/online-review.page.jsx
+++ src/pages/public/online-review/online-review.page.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import React, { Fragment, useEffect } from 'react'
+
+import Header from '../../../components/public/online-review/header/header.component'
+import Guideline from '../../../components/public/online-review/guideline/guideline.component'
 
 const OnlineReviewPage = () => {
-  return <div>Online Review Page</div>
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [])
+
+  return (
+    <Fragment>
+      <Header />
+      <Guideline />
+    </Fragment>
+  )
 }
 
 export default OnlineReviewPage
