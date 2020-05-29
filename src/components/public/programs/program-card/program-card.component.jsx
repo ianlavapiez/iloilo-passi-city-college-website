6b106768806d44e3d6b18b8d95439c1172cd01b7
ianlavapiez <ianlavapiez@gmail.com>
2020-05-29 14:42:09 +0800

    [components]: added addtional row program card component

--- src/components/public/programs/program-card/program-card.component.jsx
+++ src/components/public/programs/program-card/program-card.component.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-const ProgramCard = ({ course, programs, price }) => {
+const ProgramCard = ({ course, programs, price, imageId }) => {
+  const pictureClass = `card__picture card__picture--${imageId}`
+
   return (
     <div className='col-1-of-3 u-margin-bottom-small'>
       <div className='card'>
         <div className='card__side card__side--front'>
-          <div className='card__picture card__picture--1'>&nbsp;</div>
+          <div className={pictureClass}>&nbsp;</div>
           <h4 className='card__heading'>
             <span className='card__heading-span card__heading-span--1'>
               {course}
