5080e2eac0ca8512982e76a706fef93e6ea178f6
ianlavapiez <ianlavapiez@gmail.com>
2020-05-29 14:40:15 +0800

    [data]: added static programs data

---
+++ src/data/public/programs/programs.js
@@ -0,0 +1,71 @@
+export const programsOne = [
+  {
+    course: 'BSCRIM',
+    programs: [
+      'Bachelor of Science in Criminology',
+      'Refresher',
+      'Enhancement',
+      'Intensive',
+    ],
+    price: 'P5,000.00',
+    imageId: '1',
+  },
+  {
+    course: 'BSD',
+    programs: [
+      'Bachelor of Science in Dentistry',
+      'Refresher',
+      'Enhancement',
+      'Intensive',
+    ],
+    price: 'P5,000.00',
+    imageId: '2',
+  },
+  {
+    course: 'BSMT',
+    programs: [
+      'Bachelor of Science in Medical Technology',
+      'Refresher',
+      'Enhancement',
+      'Intensive',
+    ],
+    price: 'P5,000.00',
+    imageId: '3',
+  },
+]
+
+export const programsTwo = [
+  {
+    course: 'BSN',
+    programs: [
+      'Bachelor of Science in Nursing',
+      'Refresher',
+      'Enhancement',
+      'Intensive',
+    ],
+    price: 'P5,000.00',
+    imageId: '4',
+  },
+  {
+    course: 'BSPT',
+    programs: [
+      'Bachelor of Science in Physical Therapy',
+      'Refresher',
+      'Enhancement',
+      'Intensive',
+    ],
+    price: 'P5,000.00',
+    imageId: '5',
+  },
+  {
+    course: 'BSRT',
+    programs: [
+      'Bachelor of Science in Radio Technology',
+      'Refresher',
+      'Enhancement',
+      'Intensive',
+    ],
+    price: 'P5,000.00',
+    imageId: '6',
+  },
+]
