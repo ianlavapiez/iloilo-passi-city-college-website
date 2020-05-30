361ffc4de51744bb7c5e5cceb94dbf8b80347220
ianlavapiez <ianlavapiez@gmail.com>
2020-05-30 23:27:28 +0800

    [redux]: modified redux

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -5,6 +5,7 @@ import storage from 'redux-persist/lib/storage'
 import userReducer from './user/user.reducer'
 import messageReducer from './public/message/message.reducer'
 import studentReducer from './student/student.reducer'
+import accountingReducer from './accounting/accounting.reducer'
 
 const persistConfig = {
   key: 'root',
@@ -16,6 +17,7 @@ const rootReducer = combineReducers({
   user: userReducer,
   publicMessage: messageReducer,
   students: studentReducer,
+  payments: accountingReducer,
 })
 
 export default persistReducer(persistConfig, rootReducer)
