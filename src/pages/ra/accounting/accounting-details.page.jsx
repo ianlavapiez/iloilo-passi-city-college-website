a1ab213aaa6f08bb8a177bd18f2be03c142d88ec
ianlavapiez <ianlavapiez@gmail.com>
2020-06-07 22:24:48 +0800

    [pages]: added new accounting details page

---
+++ src/pages/ra/accounting/accounting-details.page.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import { Layout, Breadcrumb, Typography } from 'antd'
+
+import Sidebar from '../../../components/ra/sidebar/sidebar.component'
+import Navbar from '../../../components/ra/navbar/navbar.component'
+import RaFooter from '../../../components/ra/footer/footer.component'
+import PaymentManagementInfoTable from '../../../components/ra/payment-management-info-table/payment-management-info-table.component'
+
+const { Content } = Layout
+const { Title } = Typography
+
+const AccountingDetailsPage = () => {
+  return (
+    <Layout style={{ minHeight: '100vh' }}>
+      <Sidebar number={'2'} />
+      <Layout className='site-layout'>
+        <Navbar />
+        <Layout style={{ padding: '0 24px 24px' }}>
+          <Breadcrumb style={{ margin: '40px 0px -20px 30px' }}>
+            <Breadcrumb.Item>
+              <Title level={3}>Payment Details</Title>
+            </Breadcrumb.Item>
+          </Breadcrumb>
+          <Content
+            className='site-layout-background'
+            style={{
+              padding: 24,
+              margin: 0,
+              minHeight: 280,
+            }}
+          >
+            <PaymentManagementInfoTable />
+          </Content>
+        </Layout>
+        <RaFooter />
+      </Layout>
+    </Layout>
+  )
+}
+
+export default AccountingDetailsPage
