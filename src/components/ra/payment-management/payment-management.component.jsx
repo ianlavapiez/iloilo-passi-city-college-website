780dd27a56c64e9dee1d2f8e859d557bf0093d77
ianlavapiez <ianlavapiez@gmail.com>
2020-06-08 21:40:10 +0800

    [components]: revised payment management component

--- src/components/ra/payment-management/payment-management.component.jsx
+++ src/components/ra/payment-management/payment-management.component.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Layout } from 'antd'
 
 import PaymentManagementTable from '../payment-management-table/payment-management-table.component'
@@ -7,12 +7,26 @@ import PaymentManagementModal from '../payment-management-modal/payment-manageme
 const { Content } = Layout
 
 const PaymentManagement = () => {
+  const [edit, setEdit] = useState(false)
+  const [data, setData] = useState('')
+  const [modalVisible, setModalVisible] = useState(false)
+
   return (
     <Content className='site-layout-background' style={{ marginTop: 10 }}>
       <Layout style={{ marginBottom: '12px' }}>
-        <PaymentManagementModal />
+        <PaymentManagementModal
+          edit={edit}
+          data={data}
+          modalVisible={modalVisible}
+          setModalVisible={setModalVisible}
+          setEdit={setEdit}
+        />
       </Layout>
-      <PaymentManagementTable />
+      <PaymentManagementTable
+        setEdit={setEdit}
+        setData={setData}
+        setModalVisible={setModalVisible}
+      />
     </Content>
   )
 }
