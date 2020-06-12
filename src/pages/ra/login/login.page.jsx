c9090778e087f885659cf6f47bf790cd0c16db71
ianlavapiez <ianlavapiez@gmail.com>
2020-06-12 23:56:54 +0800

    [pages]: modified ra login page

--- src/pages/ra/login/login.page.jsx
+++ src/pages/ra/login/login.page.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment, useState, useEffect } from 'react'
+import React, { Fragment } from 'react'
 import { Row, Col, Spin } from 'antd'
 import { connect } from 'react-redux'
 
@@ -6,30 +6,17 @@ import './login.styles.scss'
 
 import LoginForm from '../../../components/ra/login/login.component'
 import LoginSideImage from '../../../components/ra/login-side-image/login-side-image.component'
-import { popupMessageDialog } from '../../../components/common/popup-message/popup-message.component'
 
 const LoginPage = ({ loading, error }) => {
-  useEffect(() => {
-    setErrorMessage(error)
-  }, [error])
-
-  const [errorMessage, setErrorMessage] = useState(error)
-
-  const popupErrorMessageDialog = () => {
-    popupMessageDialog('warning', 'error')
-    setErrorMessage(null)
-  }
-
   return (
     <Fragment>
-      {errorMessage ? popupErrorMessageDialog() : null}
       <Spin tip='Signing in...' spinning={loading} delay={500}>
         <Row className='container'>
           <Col span={12}>
             <LoginSideImage />
           </Col>
           <Col span={12}>
-            <LoginForm error={error} />
+            <LoginForm />
           </Col>
         </Row>
       </Spin>
@@ -37,9 +24,10 @@ const LoginPage = ({ loading, error }) => {
   )
 }
 
-const mapStateToProps = (state) => ({
-  error: state.user.error,
-  loading: state.user.isLoading,
-})
+const mapStateToProps = (state) => {
+  return {
+    loading: state.async.loading,
+  }
+}
 
 export default connect(mapStateToProps)(LoginPage)
