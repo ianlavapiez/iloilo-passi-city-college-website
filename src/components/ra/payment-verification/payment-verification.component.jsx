65482b71c8e253ed9405e1ff88e8f072080fe654
ianlavapiez <ianlavapiez@gmail.com>
2020-06-12 23:58:34 +0800

    [components]: modified components

--- src/components/ra/payment-verification/payment-verification.component.jsx
+++ src/components/ra/payment-verification/payment-verification.component.jsx
@@ -1,15 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Layout } from 'antd'
 
+import PaymentVerificationModal from '../payment-verification-modal/payment-verification-modal.component'
 import PaymentVerificationTable from '../payment-verification-table/payment-verification-table.component'
 
 const { Content } = Layout
 
 const PaymentVerification = () => {
+  const [modalVisible, setModalVisible] = useState(false)
+  const [data, setData] = useState([])
+
   return (
     <Content className='site-layout-background'>
       <Layout style={{ marginBottom: '12px' }}>
-        <PaymentVerificationTable />
+        <PaymentVerificationModal
+          modalVisible={modalVisible}
+          setModalVisible={setModalVisible}
+          data={data}
+        />
+        <PaymentVerificationTable
+          setModalVisible={setModalVisible}
+          setData={setData}
+        />
       </Layout>
     </Content>
   )
