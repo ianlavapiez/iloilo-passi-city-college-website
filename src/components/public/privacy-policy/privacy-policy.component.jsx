1753d66b4c8ae3875ee37305e8df8bdce073c90c
ianlavapiez <ianlavapiez@gmail.com>
2020-06-17 15:33:44 +0800

    [components]: modified privacy policy component

--- src/components/public/privacy-policy/privacy-policy.component.jsx
+++ src/components/public/privacy-policy/privacy-policy.component.jsx
@@ -1,7 +1,33 @@
 import React from 'react'
 
+import Header from './header.component'
+import InterpretationAndDefinitions from './interpretation-and-definitions.component'
+import CollectingAndUsingYourPersonalData from './collecting-and-using-your-personal-data.component'
+import UseOfYourPersonalData from './use-of-your-personal-data.component'
+import RetentionOfYourPersonalData from './retention-of-your-personal-data.component'
+import TransferOfYourPersonalData from './transfer-of-your-personal-data.component'
+import DisclosureOfYourPersonalData from './disclosure-of-your-personal-data.component'
+import SecurityOfYourPersonalData from './security-of-your-personal-data.component'
+import LinksToOtherWebsites from './links-to-other-websites.component'
+import ChangesToThisPrivacyPolicy from './changes-to-this-privacy-policy.component'
+import ContactUs from './contact-us.component'
+
 const PrivacyPolicy = () => {
-  return <div>Privacy Policy Component</div>
+  return (
+    <div className='row'>
+      <Header />
+      <InterpretationAndDefinitions />
+      <CollectingAndUsingYourPersonalData />
+      <UseOfYourPersonalData />
+      <RetentionOfYourPersonalData />
+      <TransferOfYourPersonalData />
+      <DisclosureOfYourPersonalData />
+      <SecurityOfYourPersonalData />
+      <LinksToOtherWebsites />
+      <ChangesToThisPrivacyPolicy />
+      <ContactUs />
+    </div>
+  )
 }
 
 export default PrivacyPolicy
