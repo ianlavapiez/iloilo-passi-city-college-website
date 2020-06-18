c97150139dabdcd65b63e66f0f595865d842111c
ianlavapiez <ianlavapiez@gmail.com>
2020-06-18 13:33:46 +0800

    [redux]: modified students reducers

--- src/redux/students/students.reducers.js
+++ src/redux/students/students.reducers.js
@@ -1,8 +1,12 @@
 import { createReducer } from '../utils/reducer.utils'
-import { FETCH_STUDENT_USER } from './students.constants'
+import {
+  FETCH_STUDENT_USER,
+  FETCH_SPECIFIC_STUDENT_USER,
+} from './students.constants'
 
 const initialState = {
   students: [],
+  currentStudent: [],
 }
 
 const fetchUsers = (state, payload) => {
@@ -12,6 +16,14 @@ const fetchUsers = (state, payload) => {
   }
 }
 
+const fetchStudentDetails = (state, payload) => {
+  return {
+    ...state,
+    currentStudent: payload.students,
+  }
+}
+
 export default createReducer(initialState, {
   [FETCH_STUDENT_USER]: fetchUsers,
+  [FETCH_SPECIFIC_STUDENT_USER]: fetchStudentDetails,
 })
