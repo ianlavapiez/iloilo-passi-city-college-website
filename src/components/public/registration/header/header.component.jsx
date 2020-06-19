4c1c17d726fa83e4735fd4b7957b25c1b31324ad
ianlavapiez <ianlavapiez@gmail.com>
2020-06-19 23:14:01 +0800

    [components]: modified public component

--- src/components/public/registration/header/header.component.jsx
+++ src/components/public/registration/header/header.component.jsx
@@ -6,7 +6,7 @@ const Header = () => {
       style={{ width: '100%', textAlign: 'center' }}
       className='heading-secondary u-margin-bottom-medium u-margin-top-medium'
     >
-      Be a part of the Brainhub Family!
+      Be part of the Brainhub Family!
     </span>
   )
 }
